9b98e4d6e31c0bb958ea5f5bc9f022d02cc3568e
feng lv <fenglv15@mails.ucas.ac.cn>
2017-09-13 15:04:43 +0800

    Site updated: 2017-09-13 15:04:42

--- scripts/vim_config.sh
+++ scripts/vim_config.sh
@@ -1,20 +1,43 @@
-git clone https://github.com/ucasFL/config_file.git
-sudo cp config_file/vimrc /etc/vim
-sudo rm -r config_file
-mkdir -p ~/.vim/autoload ~/.vim/bundle
-curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
-git clone https://github.com/tomasr/molokai.git
-mv molokai/colors ~/.vim
-sudo rm -r molokai
-cd ~/.vim/bundle
-git clone https://github.com/jiangmiao/auto-pairs.git
-git clone https://github.com/scrooloose/nerdcommenter.git
-git clone https://github.com/scrooloose/nerdtree.git
-git clone https://github.com/ervandew/supertab.git
-git clone --depth=1 https://github.com/vim-syntastic/syntastic.git
-git clone https://github.com/majutsushi/tagbar.git
-git clone https://github.com/Lokaltog/vim-powerline.git
-git clone https://github.com/drmingdrmer/xptemplate.git
-git clone https://github.com/kien/ctrlp.vim.git
-git clone https://github.com/unblevable/quick-scope.git
+which git;
+a=$?;
+which curl;
+b=$?;
+if [ $a -eq 0 && $b -eq 0  ]; then
+	echo "OK, everything done!";
+elif [ $a -eq 0 && $b -ne 0 ]; then
+	echo "Sorry, curl is not installed. Please install curl first.";
+	sudo apt update;
+	sudo apt install curl;
+elif [ $a -ne 0 && $b -eq 0 ]; then
+	echo "Sorry, git is not installed. Please install git first."
+	sudo apt update;
+	sudo apt install git;
+else
+	echo "Both git and curl are not installed. Please install them first.";
+	sudo apt update;
+	sudo apt install git;
+	sudo apt install curl;
+fi
+echo "Begin configuring Vim......"
 
+git clone https://github.com/ucasFL/config_file.git;
+sudo cp config_file/vimrc /etc/vim;
+sudo rm -r config_file;
+mkdir -p ~/.vim/autoload ~/.vim/bundle;
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim;
+git clone https://github.com/tomasr/molokai.git;
+mv molokai/colors ~/.vim;
+sudo rm -r molokai;
+cd ~/.vim/bundle;
+git clone https://github.com/jiangmiao/auto-pairs.git;
+git clone https://github.com/scrooloose/nerdcommenter.git;
+git clone https://github.com/scrooloose/nerdtree.git;
+git clone https://github.com/ervandew/supertab.git;
+git clone https://github.com/vim-syntastic/syntastic.git;
+git clone https://github.com/majutsushi/tagbar.git;
+git clone https://github.com/Lokaltog/vim-powerline.git;
+git clone https://github.com/drmingdrmer/xptemplate.git;
+git clone https://github.com/kien/ctrlp.vim.git;
+git clone https://github.com/unblevable/quick-scope.git;
+
+echo "Configuring Vim completed! Enjoying your Vim."
