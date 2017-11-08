f7a8fb2969216b06254fa4b1a0a0ea5fb4bc0533
feng lv <fenglv15@mails.ucas.ac.cn>
2017-11-08 12:53:51 +0800

    Site updated: 2017-11-08 12:53:49

---
+++ js/src/op.js
@@ -0,0 +1,14 @@
+function detectOS() {
+  var sUserAgent = navigator.userAgent;
+  var isWin = (navigator.platform == "Win32") || (navigator.platform == "Windows");
+  var isMac = (navigator.platform == "Mac68K") || (navigator.platform == "MacPPC") || (navigator.platform == "Macintosh") || (navigator.platform == "MacIntel");
+  if (isMac) return "Mac";
+  var isUnix = (navigator.platform == "X11") && !isWin && !isMac;
+  if (isUnix) return "Unix";
+  var isLinux = (String(navigator.platform).indexOf("Linux") > -1);
+  if (isLinux) return "Linux";
+  if (isWin) return "Windows";
+  return "other";
+}
+
+document.writeln("您的操作系统是：" + detectOS());
